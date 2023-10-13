f9e4caaaf1e845f41231bde46d736fac903d8607
haram <147386503+project-main@users.noreply.github.com>
2023-10-13 18:15:55 +0900

    Delete po_5 directory
    오류내역 삭제

--- po_5/src/main/java/com/board/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.board.service;
-
-import com.board.domain.UserDTO;
-
-public interface UserService {
-	public boolean insertUser(UserDTO params);
-	boolean loginUser(UserDTO params);
-}
--- po_5/src/main/resources/static/script/main.js
+++
@@ -1,3 +0,0 @@
-function signUp()   {window.location.href = "/signUp";}
-function login()  {window.location.href = "/login";}
-function logout() {window.location.href = "/logout.do";}
